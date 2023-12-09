e1b92b6111fe7df2a4142b2dad3605948f3b5507
dtaisfranca <127639116+dtaisfranca@users.noreply.github.com>
2023-12-09 19:26:33 -0300

    Update Observar.java

--- Biblioteca/src/biblioteca/Observar.java
+++ Biblioteca/src/biblioteca/Observar.java
@@ -1,5 +1,53 @@
 package biblioteca;
 
-public class Observar {
+public interface Observer {
+	public void update(LivrosObservados livro);
+}
+=========================================
+  
+  
+public interface Subject {
+	public void registerObserver(Observer o);
+	public void removeObserver(Observer o);
+	public void notifyObservers();
+}
 
+================================================
+
+public class LivrosObservados implements Subject {
+	private ArrayList<Observer> observers;
+	
+	
+public LivrosObservados() {
+		observers = new ArrayList<Observer>();
+	}
+	
+	public void registerObserver(Observer o) {
+		observers.add(o);
+	}
+	
+	public void removeObserver(Observer o) {
+		int i = observers.indexOf(o);
+		if (i >= 0) {
+			observers.remove(i);
+		}
+	}
+	
+	public void notifyObservers() {
+		for (int i = 0; i < observers.size(); i++) {
+			Observer observer = observers.get(i);
+			observer.update(this);
+		}
+	}
+	
+===================================================
+
+  
+public class ProfessoresObservadores implements Observer
+ {
+
+   
+	public void update(LivrosObservados livros) {
+	
+	}
 }
